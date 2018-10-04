adfafbad1f69e839b9022f3d8dded6a62252e6ef
System Administrator <root@MacBook-Pro-de-joyce.local>
2018-10-04 17:26:46 -0500

    diseño de presentacion (home)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<h3>{{ message }}</h3>`
+  templateUrl: './home.component.html', 
+  styleUrls:['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   public message: string;
